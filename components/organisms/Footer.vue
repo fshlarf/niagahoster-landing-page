eae8f0f4952e6fec141fb4082ca85fafac305770
fshlarf <faishal.arif218@gmail.com>
2020-06-19 14:17:34 +0700

    add element footer

--- components/organisms/Footer.vue
+++ components/organisms/Footer.vue
@@ -2,7 +2,7 @@
     <div class="footer">
         <div class="container">
             <div class="footer__row">
-                <div>
+                <div class="coloum">
                     <h5>HUBUNGI KAMI</h5>
                     <ListInfo
                         :arrayModule="dataCompany1"
@@ -12,19 +12,19 @@
                         :arrayModule="dataCompany2"
                     />
                 </div>
-                <div>
+                <div class="coloum">
                     <h5>LAYANAN</h5>
                     <ListInfo
                         :arrayModule="services"
                     />
                 </div>
-                <div>
+                <div class="coloum">
                     <h5>SERVIS HOSTING</h5>
                     <ListInfo
                         :arrayModule="serviceHosting"
                     />
                 </div>
-                <div>
+                <div class="coloum">
                     <h5>TUTORIAL</h5>
                     <ListInfo
                         :arrayModule="tutorial"
@@ -32,17 +32,30 @@
                 </div>
             </div>
             <div class="footer__row">
-                <div>
+                <div class="coloum">
                     <h5>TENTANG KAMI</h5>
                     <ListInfo
                         :arrayModule="aboutUs"
                     />
                 </div>
-                <div>
+                <div class="coloum">
                     <h5>KENAPA PILIH NIAGAHOSTER?</h5>
                     <ListInfo
-                        :arrayModule="aboutUs"
+                        :arrayModule="reasons"
                     />
+                </div>
+                <div class="footer__newsletter coloum">
+                    <h5>NEWSLETTER</h5>
+                    <div class="footer__newsletter-content">
+                        <input placeholder="Email"/>
+                        <button type="button" class="btn btn-primary">Berlangganan</button>
+                    </div>
+                    <p class="footer__newsletter-text">Dapatkan promo dan konten menarik <br>dari penyedia hosting terbaik Anda.</p>
+                </div>
+                <div class="footer__credentials coloum">
+                    <i class="fa fa-facebook fb"></i>  
+                    <i class="fa fa-twitter tw"></i>  
+                    <i class="fa fa-google-plus gp"></i>  
                 </div>
             </div>
         </div>
@@ -62,7 +75,8 @@ export default {
             services: ['Domain', 'Shared Hosting', 'Cloud VPS Hosting', 'Managed VPS Hosting', 'Web Builder', 'Keamanan SSL/HTTPS', 'Jasa Pembuatan Website', 'Program Affiliasi'],
             serviceHosting: ['Hosting Murah', 'Hosting Indonesia', 'Hosting Singapura SG', 'Hosting PHP', 'Hosting Wordpress', 'Hosting Laravel'],
             tutorial: ['Knowledgebase', 'Blog', 'Cara Pembayaran'],
-            aboutUs: ['Tim Niagahoster', 'Karir', 'Events', 'Penawaran & Promo Spesial', 'Kontak']
+            aboutUs: ['Tim Niagahoster', 'Karir', 'Events', 'Penawaran & Promo Spesial', 'Kontak'],
+            reasons: ['Support Terbaik', 'Garansi Harga Termurah', 'Domain Gratis Selamanya', 'Datacenter Hosting Terbaik', 'Review Pelanggan']
         }
     }
 }
@@ -73,9 +87,11 @@ export default {
     padding: 60px 0px 30px 0px;
     background-color: #303030;
     color: white!important;
+    font-size: 14px!important;
     h5 {
         padding: 16px!important;
         margin-bottom: 16px;
+        font-size: 16px;
     }
     .container {
         max-width: 1120px!important;
@@ -87,6 +103,54 @@ export default {
     &__row {
         display: flex;
         margin-bottom: 70px;
+    }
+    &__newsletter {
+        h5 {
+            margin-left: -10px;
+        }
+        &-content {
+            background-color: white;
+            padding: 5px 4px;
+            width: fit-content;
+            border-radius: 25px;
+            margin-bottom: 10px;
+            button {
+                padding: 10px 10px;
+            }
+            input {
+                background-color: transparent!important;
+                border: none!important;
+                width: 100px!important;
+                padding: 0px 16px!important;
+            }
+        }
+        p {
+            font-size: 13px;
+        }
+    }
+    &__credentials {
+        display: flex;
+        height: max-content;
+        margin-left: 20px;
+        i {
+            border: 1px solid white;
+            border-radius: 50px;
+            font-size: 26px;
+            margin: 72px 10px;
+        }
+        .fb {
+            padding: 18px 23px;
+        }
+        .tw {
+            padding: 18px;
+        }
+        .gp {
+            padding: 18px 14px;
+        }
+    }
+    .coloum {
+        width: 30%;
+        text-align: left;
     }
 }
 </style>
